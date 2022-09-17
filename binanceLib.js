8d02aa197e6c609d0f974d34598c6dcc55717422
GTedZ <Hellis_over@icloud.com>
2022-09-17 14:41:48 +0300

    update

--- binanceLib.js
+++ binanceLib.js
@@ -4,39 +4,50 @@ module.exports = everything;
 // timeInForce is GTX for post, and GTC for limit orders apparently
 
 function everything(APIKey, APISecret) {
+    const axios = require('axios')
     const crypto = require('crypto');
     const recvWindow = 5000;
-    const APIKey = APIKey, APISecret = APISecret;
+    var APIKEY = APIKey, APISECRET = APISecret;
+    console.log({ APIKEY }, '\n', { APISECRET })
+
+    let base = 'https://api.binance.com';
+    let wapi = 'https://api.binance.com';
+    let sapi = 'https://api.binance.com';
+    let fapi = 'https://fapi.binance.com';
+    let dapi = 'https://dapi.binance.com';
 
     return {
-        futuresMarketBuy: async (symbol, quantity) => {
+        futuresMarketBuy: async function (symbol, quantity) {
             let params = {
                 symbol: symbol,
                 quantity: quantity,
                 side: 'BUY',
                 type: 'MARKET',
-                timeInForce: 'GTX'
+                // timeInForce: 'GTX',
+                newOrderRespType: "RESULT"
             }
 
-            return this.createFuturesOrder(params,)
+            return this.createFuturesOrder(params)
         },
 
-        createFuturesOrder: async (params) => {
-            let query = '', headers = {
-                'User-Agent': userAgent,
+        createFuturesOrder: async function (params) {
+            let baseURL = fapi + '/fapi/v1/order';
+            let query = '', signature, URL, headers = {
                 'Content-type': 'application/x-www-form-urlencoded'
             };
-            headers['X-MBX-APIKEY'] = APIKey;
+            headers['X-MBX-APIKEY'] = APIKEY;
 
             params.recvWindow = recvWindow;
-            params.timestamp = new Date().getTime() + Binance.info.timeOffset;
+            params.timestamp = new Date().getTime() + 1000;
             query = this.makeQueryString(params);
-
+            signature = crypto.createHmac('sha256', APISECRET).update(query).digest('hex'); // HMAC hash header
+            URL = `${baseURL}?${query}&signature=${signature}`;
             console.log(query)
+            return this.sendRequest(URL, 'post', headers);
         },
 
         makeQueryString: (q) => {
-            Object.keys(q)
+            return Object.keys(q)
                 .reduce((a, k) => {
                     if (Array.isArray(q[k])) {
                         q[k].forEach(v => {
@@ -49,6 +60,14 @@ function everything(APIKey, APISecret) {
                 }, [])
                 .join("&");
 
+        },
+
+        sendRequest: async function (URL, method, headers) {
+            try {
+                return await axios[method](URL, '', { headers: headers })
+            } catch (err) {
+                return err
+            }
         }
     }
 }
